a85c7835d0ecfa94778b7e657c337fa26310a78b
Sardorxoliqulovv <sardorxoliqulovv@gmail.com>
2026-06-15 13:41:30 +0500

    initioal commit

--- index.js
+++ index.js
@@ -1,3 +1,8 @@
-setTimeout(function() {
-  console.log('men 2 secund dan keyin ishliman')
-}, 2000)
+let decriment = 0;
+function setimeout() {
+  setTimeout(function() {
+    console.log('tartib ' + decriment++)
+    setimeout();
+  }, 2000);
+}
+setimeout();
